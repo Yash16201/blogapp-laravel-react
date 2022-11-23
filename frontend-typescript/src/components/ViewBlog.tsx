dc5cf78bb74097aa2d3d473a78c5b848b1433e07
Yash16201 <modiy2433@gmail.com>
2022-11-23 12:44:35 +0530

    Warning fix

--- frontend-typescript/src/components/ViewBlog.tsx
+++ frontend-typescript/src/components/ViewBlog.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import  {useEffect} from 'react'
 import { RootState, AppDispatch } from "../store";
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
